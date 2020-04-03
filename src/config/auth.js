e62c1380704e59c5d078b6f88b11a511c05594cc
skye <ssy@yopoint.com>
2020-04-03 15:40:27 +0800

    登录时间改为15天有效

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 const authConfig = {
   isProduction: process.env.XALD_MANAGEMENT_IS_PRODUCTION == "true",
   secret: process.env.XALD_MANAGEMENT_APP_SECRET,
-  tokenExpiryTime: 60 * 60, // seconds => 5 minutes
+  tokenExpiryTime: 60 * 60 * 24 * 15, // seconds => 5 minutes
   redisServerPort: process.env.XALD_MANAGEMENT_REDIS_PORT || 6379,
   redisServerURL: process.env.XALD_MANAGEMENT_REDIS_URL,
   redisConnectionString: process.env.REDIS_URL,
